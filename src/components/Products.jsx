7a1a4b5cfc8ee96183e2c73888e4454a8fb1e9e0
OciuzreactDev1 <reactdeveloper@ociuz.in>
2024-05-09 12:06:29 +0530

    feat(rtk): implement createApi

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { productThunk } from '../redux/productThunk'
-import { useGetAllProductsQuery } from '../redux/createApi.demo'
+import { useGetAllProductsQuery, useGetProductByIdQuery } from '../redux/createApi.demo'
 
 export default function Products() {
     const dispatch = useDispatch()
     const products = useSelector((state) => state.products.products)
-    const error = useSelector((state) => state.products.error)
+    // const error = useSelector((state) => state.products.error)
     const status = useSelector((state) => state.products.status)
 
     const fetchProducts = () => {
         dispatch(productThunk(10));
     }
 
-    const { data } = useGetAllProductsQuery()
+    const { data, isLoading, refetch, isFetching, error, isError, isSuccess } = useGetProductByIdQuery(10)
     console.log("Data: ", data);
+    console.log(isFetching);
 
-    console.log(products);
+    // console.log(products);
     // console.log(error);
     // console.log(status);
     return (
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./productSlice";
-import loggingMiddleware from "./logginMiddleware";
 import { api } from "./createApi.demo";
 
 export const store = configureStore({
     reducer: {
-        products: productSlice,
         [api.reducerPath]: api.reducer
     },
-    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), loggingMiddleware, api.middleware]
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), api.middleware]
 })
