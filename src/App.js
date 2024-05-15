279afcb3589906ed466e91f21bf50e5421928dae
OciuzreactDev1 <reactdeveloper@ociuz.in>
2024-05-15 13:58:40 +0530

    chore: Add Bootstrap CSS for styling
    ```

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FetchProducts from './components/FetchProduct';
 
 function App() {
   return (
-    <Products />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Products />} />
+        <Route path='/fetchProducts' element={<FetchProducts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
